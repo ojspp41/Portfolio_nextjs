8eeb425f40b5c38cd6f8b73d3628602d1930b5ea
ohjunseok <ojspp000@naver.com>
2024-12-28 17:24:32 +0900

    수정!

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false, // SWC 압축 비활성화
+  swcMinify: true, // SWC 압축 활성화로 변경
   images: {
     domains: [
       'www.notion.so',
       'images.unsplash.com',
       's3.us-west-2.amazonaws.com',
-      'prod-files-secure.s3.us-west-2.amazonaws.com'
+      'prod-files-secure.s3.us-west-2.amazonaws.com',
+      'cdn.example.com', // 추가 도메인 예시
     ],
+    formats: ['image/avif', 'image/webp'], // 이미지 포맷 지원 추가
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // 클라이언트 빌드 시 특정 설정 추가 (필요한 경우)
+      config.resolve.fallback = { fs: false };
+    }
+    return config;
   },
 };
 
