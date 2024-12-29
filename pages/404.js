0b7cc104a0c7c5dae84a810e2600ea8d15b5e976
ojspp000@naver.com <ojspp000@naver.com>
2024-12-29 20:28:17 +0900

    404find

--- pages/404.js
+++ pages/404.js
@@ -1,18 +1,30 @@
-import React from 'react'
-import Lottie from 'react-lottie-player'
+import React, { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
 
-import errorAnimation from "../public/error.json";
+// dynamic import를 통해 SSR을 비활성화합니다.
+const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false });
+
+import errorAnimation from '../public/error.json';
 
 export default function PageNotFound() {
+    const [isBrowser, setIsBrowser] = useState(false);
+
+    // 클라이언트 환경 여부를 확인
+    useEffect(() => {
+        setIsBrowser(typeof window !== 'undefined');
+    }, []);
+
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <Lottie
-                autoPlay
-                loop
-                animationData={errorAnimation}
-                play
-                style={{ height: "300px", width: "300px" }}
-            />
+            {isBrowser && (
+                <Lottie
+                    autoPlay
+                    loop
+                    animationData={errorAnimation}
+                    play
+                    style={{ height: '300px', width: '300px' }}
+                />
+            )}
             <h1 className="text-2xl font-bold text-gray-700 mt-6">
                 페이지를 찾을 수 없습니다 ㅠㅠ
             </h1>
